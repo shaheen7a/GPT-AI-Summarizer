81712fd131fcffd142b2d0454e086b2439628ad2
Shaheen7a <aousshaheen7@gmail.com>
2023-04-30 03:05:39 -0700

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
-
+import "./App.css"
+import Demo from "./components/Demo"
+import Hero from "./components/Hero"
 const App = () => {
   return (
-    <div>App</div>
+    <main>
+      <div className="main">
+        <div className="gradient" />
+      </div>
+
+      <div className="app">
+      <Hero />
+      <Demo />
+      </div>
+    </main>
   )
 }
 
